2e45abc5f8ac552f284d27e62b834a74e72b3210
childishelemike <elemike2004@gmail.com>
2023-11-08 18:16:47 -0500

    cambio de icono de google y facebook

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-const $form = document.querySelector('#form')
+const $form = document.querySelector('#form');
+const formulario = document.getElementById('formulario');
+const mensaje = document.getElementById('mensaje');
+const botonIniciarSesion = document.getElementById('boton-iniciar-sesion');
 
 $form.addEventListener('submit', handleSubmit)
 
@@ -17,3 +20,42 @@ async function handleSubmit(event) {
         alert('Gracias por contactarnos, estamos trabajando para darte respuesta lo más pronto posible <3')
     }
 }
+
+
+        
+
+formulario.addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe de forma convencional
+
+    const url = 'http://localhost:8080/api/usuario/login'; // URL de inicio de sesión
+    const usuario = document.getElementById('usuario').value;
+    const contrasena = document.getElementById('contrasena').value;
+
+    const datosUsuario = {
+      usuario: usuario,
+      contrasena: contrasena
+    };
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(datosUsuario)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('No se pudo iniciar sesión');
+        }
+        return response.json();
+      })
+      .then(data => {
+        // Manejar la respuesta del servidor, por ejemplo, guardar un token de autenticación
+        const token = data.token;
+        mensaje.textContent = 'Inicio de sesión exitoso. Token: ' + token;
+      })
+      .catch(error => {
+        // Manejar errores
+        mensaje.textContent = 'Error al iniciar sesión: ' + error.message;
+      });
+  });
