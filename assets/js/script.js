e24bb40c8ea974a448f98ccabe23c71d0b98ca4a
Joscari <joscariortizc@gmail.com>
2023-11-08 14:25:13 -0500

    ult cambios

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,38 @@ async function handleSubmit(event) {
         alert('Gracias por contactarnos, estamos trabajando para darte respuesta lo más pronto posible <3')
     }
 }
+
+document.getElementById('registroForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const correo = document.getElementById('correo').value;
+
+    const datosUsuario = {
+        nombre: nombre,
+        apellido: apellido,
+        correo: correo
+    };
+
+    fetch('/api/usuario', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datosUsuario)
+    })
+    .then(response => {
+        if (response.status === 201) {
+            // El registro se ha creado exitosamente
+            alert('Registro exitoso');
+            // Puedes redirigir al usuario a una página de confirmación o hacer otras acciones
+        } else {
+            // Ocurrió un error en el servidor
+            alert('Hubo un problema durante el registro');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
